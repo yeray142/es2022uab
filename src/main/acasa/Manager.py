07667448221a7d41073d9a04f781e98a85c02d72
yeray142 <yeraycorderocarrasco@gmail.com>
2022-05-27 15:35:56 +0200

    Green phase 2
    Fase verda del segon test. S'ha modificat el codi de la funció cerca producte per a que mínimament passi el test. Sempre retornarà "False" en tot.
    
    Co-Authored-By: 1606868-uab <101974500+1606868-uab@users.noreply.github.com>

--- src/main/acasa/Manager.py
+++ src/main/acasa/Manager.py
@@ -7,6 +7,8 @@ class Manager:
         self.__external_provider = external_provider
 
     def cerca_producte(self, product_list: list, proveidor_extern: ExternalProviderService) -> (list, list):
+        disponibility = [False] * len(product_list)
+        ads = [False] * len(product_list)
         for i, v in enumerate(product_list):
             proveidor_extern.has_associated_ad(v)
-        return True
+        return disponibility, ads
