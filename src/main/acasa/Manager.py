a0843e604f6929c9986ed48c48039f6d9f9c03f6
yeray142 <yeraycorderocarrasco@gmail.com>
2022-05-27 16:32:56 +0200

    Refactor phase 5
    Fase refactor del cinquè test. S'ha modificat el codi de la funció cerca productes per a que creï un array amb tot False per després anar modificant si es True, d'aquesta manera ens estalviem un else dintre del bucle.
    
    Co-Authored-By: 1606868-uab <101974500+1606868-uab@users.noreply.github.com>

--- src/main/acasa/Manager.py
+++ src/main/acasa/Manager.py
@@ -9,14 +9,12 @@ class Manager:
         self.__product_bd = product_bd
 
     def cerca_producte(self, product_list: list) -> (list, list):
-        disponibility = []
+        disponibility = [False] * len(product_list)
         ads = []
         available_products = self.__product_bd.product_query()
 
         for i, v in enumerate(product_list):
             if v in available_products:
-                disponibility.append(True)
-            else:
-                disponibility.append(False)
+                disponibility[i] = True
             ads.append(self.__external_provider.has_associated_ad(v))
         return disponibility, ads
